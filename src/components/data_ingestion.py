0d4f8db008f3dfda7a5b4cc9687942677c1f9099
Joe Jin <jinoujoe@gmail.com>
2023-07-26 00:26:52 +0200

    'datatransformation'

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass  # useful for only defining variables in the class
 class DataIngestionConfig:
@@ -36,10 +39,15 @@ class DataIngestion:
 
             logging.info("Ingestion of data is completed")
 
+            return (self.ingestion_config.train_data_path, self.ingestion_config.test_data_path)
+
         except Exception as e:
             raise CustomException(e, sys)
 
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data_path, test_data_path = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data_path, test_data_path)
